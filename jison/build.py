683d06e140b0bdae1a7f2f3ad7683a23d3b64ab5
David Chen <mvjome@gmail.com>
2015-11-12 14:11:34 +0800

    build.py support print error logs

--- jison/build.py
+++ jison/build.py
@@ -11,8 +11,11 @@ os.system("jison human.jison")
 # 2. Run unit tests
 def unittest(case, expected_result):
     unittest_filename = case + ".human"
-    run_result = commands.getoutput("node human.js " + unittest_filename)
-    assert json.loads(run_result) == expected_result, [run_result == expected_result]
+    status, run_result = commands.getstatusoutput("node human.js " + unittest_filename)
 
+    if status is 0:
+        assert json.loads(run_result) == expected_result, [run_result == expected_result]
+    else:
+        print run_result
 
 unittest("simple", {"count": 3, "class": 'Week'})
